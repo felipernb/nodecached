f36cffbfa60ca7dfbeb52f85dac58684440d1ca2
Felipe Ribeiro <felipernb@gmail.com>
2012-03-02 23:49:37 +0100

    Functional GET and SET

--- cache.js
+++ cache.js
@@ -1,18 +1,36 @@
+var CRLF = "\r\n";
 var cache = {
 	
 	storage : {},
-	
+
 	get : function(key) {
 		key = key.trim();
 		var value = "";
 		if (this.storage[key]) {
-			value += this.storage[key] + "\r\n";
+			value += "VALUE " + key + " " + this.storage[key]['flags'] + " " + this.storage[key]['size'] + "\r\n"+this.storage[key]['value'] + CRLF;
 		}
 		return value + "END";
 	},
 
-	set : function(key, value) {
-		this.storage[key.trim()] = value.trim();
+	set : function() {
+		console.info("SET", arguments);
+		
+		var args = Array.prototype.slice.call(arguments);
+		var key = args.shift();
+		var flags = args.shift();
+		var exptime = args.shift();
+		var value = args.join(' ');
+
+		var re = /(\d+)\r\n(.*)\r\n$/
+		var match = re.exec(value);
+		
+		var size = match[1];
+		value = match[2];
+		
+		if (value.length != size) return "CLIENT_ERROR Wrong specified size";
+		
+		this.storage[key] = { 'flags': flags, 'exptime': exptime, 'size': size, 'value': value};
+		
 		return "STORED";
 	},
 
@@ -22,9 +40,9 @@ var cache = {
 		var params = cmd.toString().split(' ');
 		var command = params.shift();
 		if (this.hasOwnProperty(command)) {
-			return this[command].apply(this, params) + "\r\n";
+			return this[command].apply(this, params) + CRLF;
 		} else {
-			return "ERROR\r\n";
+			return "ERROR" + CRLF;
 		}
 	}
 
